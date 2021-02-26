c77d1fcabf7feb6166703eaeeab02c281e0c5d9d
JulienDev76-75 <julien.coart@gmail.com>
2021-02-26 12:51:33 +0100

    computer choice

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert("Bienvenue au Mortal Shifumi");
+var username = prompt("Veuillez rentrer votre nom de Kombattant :");
+alert("Merci" + " " + username);
+
+
+var tabchoice = ["Pierre", "Feuille", "Ciseaux"]; {
+var tabchoice = prompt("Veuillez choisir entre Pierre, Feuille ou Ciseaux :");
+alert("Merci" + username)
+//var joueur = tabchoice
+
+//var ordi = Math.floor(Math.random(tabchoice)*3
+
+
+}
+
